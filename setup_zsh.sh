3b629ef2c87abf2eb64727fd9f5978cb016f4bf9
Shuichi Takaya <takayashuichi-github@yahoo.co.jp>
2019-08-16 22:46:07 +0900

    zshの初期設定ファイルを作成

---
+++ setup_zsh.sh
@@ -0,0 +1,12 @@
+#!/bin/zsh
+ln -s $HOME/dotfiles/zshrc $HOME/.zshrc
+ 
+git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
+ 
+setopt EXTENDED_GLOB
+for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
+  ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
+done
+ 
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
+chsh -s /bin/zsh
