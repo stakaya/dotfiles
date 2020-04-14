3ae35a16729c96dd2c1a601bc3b94b883184e9fa
Shuichi Takaya <takayashuichi-github@yahoo.co.jp>
2020-04-14 19:45:17 +0900

    vifmのconfig位置を修正

--- setup_app.sh
+++ setup_app.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-DOT_FILES=(gvimrc vimrc vimrc.keymap ideavimrc xvimrc vim tmux.conf)
+DOT_FILES=(gvimrc vimrc vimrc.keymap ideavimrc xvimrc vim vifm tmux.conf)
 
 for file in ${DOT_FILES[@]}
 do
