6451d6508341fd7d8b07583d2cbf79e4d9e0cab4
Shuichi Takaya <takayashuichi-git@yahoo.co.jp>
2021-09-14 22:45:15 +0900

    ssh config git config を管理から外す

--- setup_shell.sh
+++ setup_shell.sh
@@ -1,15 +1,13 @@
 #!/bin/zsh
 ln -s $HOME/dotfiles/zshrc $HOME/.zshrc
-ln -s $HOME/dotfiles/gitconfig $HOME/.gitconfig
-ln -s $HOME/dotfiles/sshconfig $HOME/.ssh/config
- 
+
 git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
- 
+
 setopt EXTENDED_GLOB
 for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
   ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
 done
- 
+
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-completions ~/.zsh/zsh-completions
 chsh -s /bin/zsh
