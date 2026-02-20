8d666fcc1cb820ab26c620eb9f72143df40b9138
stakaya <takayashuichi-git@yahoo.co.jp>
2026-02-20 14:09:53 +0900

    Refactor nvm function definitions to include `function` keyword for clarity.

--- zsh/functions/nvm.zsh
+++ zsh/functions/nvm.zsh
@@ -3,9 +3,9 @@ function __load_nvm() {
   [ -s "$NVM_DIR/nvm.sh" ] && source "$NVM_DIR/nvm.sh"
 }
 
-node()     { __load_nvm; command node "$@"; }
-npm()      { __load_nvm; command npm "$@"; }
-npx()      { __load_nvm; command npx "$@"; }
-yarn()     { __load_nvm; command yarn "$@"; }
-pnpm()     { __load_nvm; command pnpm "$@"; }
-corepack() { __load_nvm; command corepack "$@"; }
+function __node()     { __load_nvm; command node "$@"; }
+function __npm()      { __load_nvm; command npm "$@"; }
+function __npx()      { __load_nvm; command npx "$@"; }
+function __yarn()     { __load_nvm; command yarn "$@"; }
+function __pnpm()     { __load_nvm; command pnpm "$@"; }
+function __corepack() { __load_nvm; command corepack "$@"; }
